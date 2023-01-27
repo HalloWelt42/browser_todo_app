0bdff3d52d6790b11fb0accfd05d15c0b576c623
HalloWelt42 <github@texeln.de>
2023-01-27 04:33:43 +0100

    refact: remove unused code

--- src/lib/Statusbar.svelte
+++ src/lib/Statusbar.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-    import DataBaseIcon from "./components/DataBaseIcon/DataBaseIcon.svelte";
-    import {States} from "./components/DataBaseIcon/States";
-    let current_state:string = States[States.ready];
+
 </script>
 
 
-<DataBaseIcon bind:current_state/>
